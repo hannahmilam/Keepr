a2a88364287bc84cd9b0a375f3e40cc71c3b5cf1
Hannah Milam <hannah.milam16@gmail.com>
2021-11-01 11:38:16 -0600

    working on vaults details page

--- Keepr.client/src/Models/Vault.js
+++ Keepr.client/src/Models/Vault.js
@@ -6,6 +6,7 @@ export class Vault{
   this.description = data.description
   this.isPrivate = data.isPrivate
   this.creator = data.creator || {}
+  this.img = data.img || "https://images.unsplash.com/photo-1616033481409-1bc22a923483?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGJhY2tncm91bmQlMjBpbWFnZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
   }
 
 }
---
+++ Keepr.client/src/components/VaultDetails.vue
@@ -0,0 +1,32 @@
+<template>
+<p>testing from vault details component</p>
+
+</template>
+
+<script>
+import { computed } from '@vue/reactivity'
+import { AppState } from '../AppState'
+import { Keep } from '../Models/Keep'
+import { Vault } from '../Models/Vault'
+export default {
+props: {
+  vault: {
+    type: Vault,
+    default: () => {return new Vault()}
+  }
+},
+ setup(){
+    return{
+      account: computed(() => AppState.account),
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+.details-img{
+  background-size: cover;
+  width: 100%;
+}
+</style>
