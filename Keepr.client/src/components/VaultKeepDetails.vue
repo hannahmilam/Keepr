e74ec0d1c4ebccb96badb9169c8ed14cb706969f
Hannah Milam <hannah.milam16@gmail.com>
2021-11-01 20:09:21 -0600

    updated vaultkeep details modal format

--- Keepr.client/src/components/VaultKeepDetails.vue
+++ Keepr.client/src/components/VaultKeepDetails.vue
@@ -1,4 +1,5 @@
  <template>
+ <div class="card">
    <div class="row">
       <div class="col-md-6">
       <img :src="keep.img" class="details-img rounded-start" alt="">
@@ -10,20 +11,40 @@
            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
           </div>
+
+         <div class="row justify-content-around mt-4">
+            <div class="col-1 text-center ms-3">
+              <h5><i class="mdi mdi-eye"></i></h5>
+            </div>
+            <div class="col-1 text-center">
+              <h5><i class="mdi mdi-alpha-k-box-outline"></i></h5>
+            </div>
+            <div class="col-1 me-5 text-center">
+               <h5><i class="mdi mdi-share-variant"></i></h5>
+            </div>
+         <div class="row mb-2 justify-content-around">
+
+        <div class="card-body my-3">
         <h2 class="text-center">{{keep.name}}</h2>
         <p class="mx-3">{{keep.description}}</p>
-      <div class="row mb-2 justify-content-around">
+        </div>
+        </div>
       
         
-        <div class="col-5 text-center m-0 p-0">
+        <div class="card-footer mt-1 p-0 bg-transaprent my-3">
+          <div class="row m-0 p-0">
+        <div class="col-8 text-center m-0 p-0">
           <button class="btn btn-outline-secondary" @click="deleteVaultKeep(keep.vaultKeepId)">Remove From Vault</button>
           </div>
-        <div class="col-5 text-center m-0 p-0">
+        <div class="col-1 text-center m-0 p-0">
           <img :src="keep.creator?.picture" height="40" class="rounded-circle action" alt="" @click="goToProfile">
+          </div>
+          </div>
           </div>
           
         </div>
         </div>
+      </div>
       </div>
       </template>
  
@@ -75,8 +96,17 @@ props: {
 </script>
 
  <style scoped>
- .details-img{
+.details-img{
   background-size: cover;
   width: 100%;
+  height: 100%;
+  max-height: 20rem;
+}
+.card{
+  min-width: 35rem;
+  max-height: 20rem;
+}
+.card-footer{
+  border: none;
 }
  </style>
